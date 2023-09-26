4d3630da507248a594d688c79117d6c2edb2a2f3
anesh00851 <anesh0085.1@gmail.com>
2023-09-26 17:38:58 +0530

    Init App13

--- src/app/demo-cognitive-analysis/demo-cognitive-analysis.component.ts
+++ src/app/demo-cognitive-analysis/demo-cognitive-analysis.component.ts
@@ -6,10 +6,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./demo-cognitive-analysis.component.scss']
 })
 export class DemoCognitiveAnalysisComponent implements OnInit {
+  polylinecoordinates="";
+  togglemouseaction: boolean=false;
+  circlelist:string[]=[];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  closemodal(modalcontent: HTMLElement) {
+    modalcontent.style.display = "none";
+  }
+
+  onmousedownanycircle(circle:HTMLElement) {
+    this.togglemouseaction = true;
+    circle.style.backgroundColor='green';
+    this.circlelist.push(circle.innerText);
+    this.polylinecoordinates = (circle.offsetLeft+30).toString()+" "+(circle.offsetTop+30).toString();
+  }
+
+  onsvgmouseup(svgref: HTMLElement){
+    this.togglemouseaction = false;
+    svgref.removeEventListener('mousemove', () => { });
+  }
+
+  onsvgmousemove(event:any) {
+    if(this.togglemouseaction){
+      this.polylinecoordinates += " "+event.offsetX.toString()+" "+event.offsetY.toString();
+    }
+  }
+
+  changecirclecolor(circle: HTMLElement,svgref: HTMLElement) {
+    if (this.togglemouseaction) {
+      circle.style.backgroundColor = 'green';
+      this.circlelist.push(circle.innerText);
+      if (this.circlelist.length == 10) {
+        console.log(this.circlelist.length);
+        this.togglemouseaction = false;
+        svgref.removeEventListener('mousemove', () => { });
+      }
+    }
+  }
+
+  removemouselistener(svgref: HTMLElement) {
+    this.togglemouseaction = false;
+    svgref.removeEventListener('mousemove', () => { });
+  }
 }
