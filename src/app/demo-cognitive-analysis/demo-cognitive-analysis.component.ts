108276727de5ff9cecbe4ce64387bf704bd79426
anesh00851 <anesh0085.1@gmail.com>
2023-09-26 23:32:12 +0530

    Init App14

--- src/app/demo-cognitive-analysis/demo-cognitive-analysis.component.ts
+++ src/app/demo-cognitive-analysis/demo-cognitive-analysis.component.ts
@@ -1,4 +1,6 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-demo-cognitive-analysis',
@@ -9,8 +11,34 @@ export class DemoCognitiveAnalysisComponent implements OnInit {
   polylinecoordinates="";
   togglemouseaction: boolean=false;
   circlelist:string[]=[];
+  destroyed = new Subject<void>();
+  currentScreenSize!: string;
+  displayNameMap = new Map([
+    [Breakpoints.XSmall, 'XSmall'],
+    [Breakpoints.Small, 'Small'],
+    [Breakpoints.Medium, 'Medium'],
+    [Breakpoints.Large, 'Large'],
+    [Breakpoints.XLarge, 'XLarge'],
+  ]);
 
-  constructor() { }
+  constructor(breakpointObserver: BreakpointObserver) {
+    breakpointObserver
+    .observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      Breakpoints.Medium,
+      Breakpoints.Large,
+      Breakpoints.XLarge,
+    ])
+    .pipe(takeUntil(this.destroyed))
+    .subscribe(result => {
+      for (const query of Object.keys(result.breakpoints)) {
+        if (result.breakpoints[query]) {
+          this.currentScreenSize = this.displayNameMap.get(query) ?? 'Unknown';
+        }
+      }
+    });
+   }
 
   ngOnInit(): void {
   }
